7f7e217e4d1069e58111695965c14667d531c968
Nitzan <bachata@grr.la>
2015-03-21 12:29:51 +0200

    Added material icons

--- Nitzan_Mat/mainController.js
+++ Nitzan_Mat/mainController.js
@@ -1,22 +1,15 @@
+"use strict";
 angular
-    .module('app', ['ngMaterial'])
+    .module('app', ['ngMaterial', 'ngMdIcons'])
     .controller('mainController', function($scope) {
         $scope.user = {
             title: 'Developer',
             email: 'ipsum@lorem.com',
-            firstName: '',
-            lastName: '' ,
-            company: 'Google' ,
-            address: '1600 Amphitheatre Pkwy' ,
-            city: 'Mountain View' ,
-            state: 'CA' ,
-            biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
-            postalCode : '94043'
         };
     })
     .config( function($mdThemingProvider){
         // Configure a dark theme with primary foreground yellow
         $mdThemingProvider.theme('docs-dark', 'default')
-            .primaryPalette('green')
+            .primaryPalette('deep-purple')
             .dark();
     });
